10554fcde09a59068c1b4a8abd533b63e1f783ba
SimonbJohnson <simonbjohnson@gmail.com>
2015-05-26 17:37:57 +0100

    adding mapping functions

--- js/dashgraph.js
+++ js/dashgraph.js
@@ -1,5 +1,17 @@
 var ld = {
 
+    _chartRegister :[],
+    _mapRegister:[],
+    
+    init: function(){
+        ld._chartRegister.forEach(function(e){
+           e.init(); 
+        });
+        ld._mapRegister.forEach(function(e){
+           e.init(); 
+        });        
+    },
+
     rowGraph: function(id){
 
         this._height=300;
@@ -10,6 +22,10 @@ var ld = {
         this._place = "";
         this._type = "";
         this._values = "";
+        this._barcolor = "steelblue";
+        this._fontcolor = "white";
+
+        ld._chartRegister.push(this);
 
         this.height = function(val){
             if(typeof val === 'undefined'){
@@ -64,14 +80,22 @@ var ld = {
                 return this;
             }        
         };
+        
+        this.barcolor = function(val){
+            if(typeof val === 'undefined'){
+                return this._barcolor;
+            } else {
+                this._barcolor=val;
+                return this;
+            }        
+        };        
 
         this.init= function(){
-            this._cf = this._initCrossfilter(this._data,this._place,this._type,this._values);
-            this._render(this._id,this._cf.typeGroup.all());
+            this.cf = this._initCrossfilter(this._data,this._place,this._type,this._values);
+            this._render(this._id,this.cf.typeGroup.all());           
         };
 
         this._initCrossfilter = function(data,place,key,value){
-            console.log(data,place,key,value);
             var cf = crossfilter(data);
 
             cf.placeDimension = cf.dimension(function(d){ return d[place]; });
@@ -84,7 +108,6 @@ var ld = {
 
 
         this._render = function(id,data){       
-
             this._properties.margin = {top: 20, right: 20, bottom: 20, left: 20};
             this._properties.width = this._width - this._properties.margin.left - this._properties.margin.right;
             this._properties.height = this._height - this._properties.margin.top - this._properties.margin.bottom;      
@@ -95,29 +118,31 @@ var ld = {
             this._properties.y = d3.scale.ordinal()
                .rangeBands([0, this._properties.height]).domain(data.map(function(d) {return d.key; }));
 
-            var chart = d3.select(id)
+            var _chart = d3.select(id)
                 .append('svg')
                 .attr('class', 'dashchart')
                 .attr('width', this._width)
-                .attr('height', this._height);
+                .attr('height', this._height)
+                .append("g")
+                .attr("transform", "translate(" + this._properties.margin.left + "," + this._properties.margin.left + ")");        
 
-            var parent = this;
+            var _parent = this;
 
-            chart.selectAll("rect")
+            _chart.selectAll("rect")
                 .data(data)
                 .enter().append("rect")
                 .attr("x", 0)
                 .attr("y", function(d){;
-                    return parent._properties.y(d.key);
+                    return _parent._properties.y(d.key);
                 })
                 .attr("width", function(d){
-                    return parent._properties.x(d.value);
+                    return _parent._properties.x(d.value);
                 })
-                .attr("height", parent._properties.y.rangeBand()-1)
+                .attr("height", _parent._properties.y.rangeBand()-1)
                 .attr("class","dashgraph-bar")
+                .attr("fill",this._barcolor)
                 .on("click",function(d){
-                    console.log(this);
-                    console.log(d);
+
                 });        
         };
     },
@@ -128,6 +153,10 @@ var ld = {
         this._geojson = "";
         this._center = [0,0];
         this._zoom = 1;
+        this._joinAttr = "";
+        this._joinAttr = "steelblue";
+        
+        ld._mapRegister.push(this);
 
         this.geojson = function(val){
             if(typeof val === 'undefined'){
@@ -154,9 +183,18 @@ var ld = {
                 this._zoom=val;
                 return this;
             }        
-        };     
+        };
+        
+        this.joinAttr = function(val){
+            if(typeof val === 'undefined'){
+                return this._joinAttr;
+            } else {
+                this._joinAttr=val;
+                return this;
+            }        
+        };        
 
-        this._initMap = function(id,geojson, center, zoom){
+        this._initMap = function(id,geojson, center, zoom, joinAttr){
 
             var base_hotosm = L.tileLayer(
                 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
@@ -174,15 +212,76 @@ var ld = {
                 layers: [base_hotosm,base_osm]
             });
 
-            var overlay_prefectures = L.geoJson(geojson,{
-            }).addTo(map);        
+            var overlay = L.geoJson(geojson,{
+            }).addTo(map);
+            
+            overlay.eachLayer(function (layer) {
+                if(typeof layer._path != 'undefined'){
+                    layer._path.id = layer.feature.properties[joinAttr];
+                } else {
+                    layer.eachLayer(function (layer2){
+                        layer2._path.id = layer.feature.properties[joinAttr];
+                    });
+                }
+            });            
 
             return map;
-        };
+        },
+        
+        this.mapUpdate = function(){
+            var mapData = [];
+            var parent = this;
+            ld._chartRegister.forEach(function(chart){
+                mapData = parent._addCF(mapData,chart.cf.typeGroup.all())
+            });
+            console.log(mapData);
+        },
+                
+        this._addCF = function (data,newdata){
+            if(data.length>0){
+                data.forEach(function(d){
+                    d.values.push[0];
+                });
+                var length = data[0].values.length-1;
+                newdata.forEach(function(d){
+                    var found = false;
+                    var i = 0;
+                    while(found===false && i<data.length){
+                        if(d.key === data[i].key){
+                            data[i].values.push(d.value);
+                            found =true;
+                        }
+                        i++;
+                    }
+                    if(found===false){
+                        var values = [];
+                        for (i = 0; i < length-1; i++) { 
+                            values = values.push(0);
+                        }
+                        values.push(d.value);
+                        data.push[{key:d.key,values:values}];
+                    }
+                });
+            } else {
+                newdata.forEach(function(d){
+                    data.push({key:d.key,values:[d.value]});
+                });
+            }
+            return data;
+        },
+        
+        this._colorAccessor = function (d,i){
+            var total=0;
+            for(var i in d.values) { total += d.values[i]; }
+            if(total>0){
+                return 1;
+            } else {
+                0;
+            }
+        },
 
         this.init = function(){
-            this._initMap(this._id,this._geojson,this._center,this._zoom);
+            this._initMap(this._id,this._geojson,this._center,this._zoom,this._joinAttr);
         };
     }
-    
 };
--- js/site.js
+++ js/site.js
@@ -1,9 +1,23 @@
+nepal_adm3.features.forEach(function(e){
+    e.properties['HLCIT_CODE'] = e.properties['HLCIT_CODE'].replace(/ /g,'_');
+});
 
+var dashmap = new ld.map('#map')
+        .geojson(nepal_adm3)
+        .center([28.3,83.7])
+        .zoom(6)
+        .joinAttr('HLCIT_CODE');
 
-var dashmap = new ld.map('#map').geojson(nepal_adm3).center([28.3,83.7]).zoom(6);
-dashmap.init();
-var dashgraphs=[];
-dashgraphs.push(new ld.rowGraph('#graph1').data(data).place('column1').type('column2').values('column3').width($('#graph1').width()));
-dashgraphs.push(new ld.rowGraph('#graph2').data(data).place('column1').type('column2').values('column3').width($('#graph2').width()));
-dashgraphs[0].init();
-dashgraphs[1].init();
+var rowChart1 = new ld.rowGraph('#graph1').data(data)
+        .place('column1')
+        .type('column2')
+        .values('column3')
+        .width($('#graph1').width());
+
+var rowChart2 = new ld.rowGraph('#graph2').data(data)
+        .place('column1')
+        .type('column2')
+        .values('column3')
+        .width($('#graph2').width());
+
+ld.init();
